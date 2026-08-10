01d221ac0ad7425bb96b752ba4d0a877065c5850
amir <amir.hj1998620@gmail.com>
2026-08-10 20:51:58 +0330

    renaming extract module

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from ETL.Extract import extract_data
+from ETL.extract import extract_data
 
 
 if __name__ == "__main__":
     sales_data, stores_data, features_data = extract_data()
-    print(sales_data.head())
 
